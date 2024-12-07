2d1e2117bd92909618d92f3683c0f3ca1c561866
eduardoezequieel <riv.edu10@gmail.com>
2024-12-07 13:50:43 -0600

    feat: change program start point

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace Desafio2_PED_EduardoLopez
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new DashboardView());
+            Application.Run(new SignInView());
         }
     }
 }
